96c3e29fbffa73b2fb5daf1ad1a9c0732a167c5f
Serj52 <blackday52@mail.ru>
2023-06-26 22:19:25 +0300

    added models

--- app/eventapp/forms.py
+++ app/eventapp/forms.py
@@ -1,4 +1,12 @@
 from django import forms
 
+
 class CreateEvent(forms.Form):
-    name = forms.CharField(label='Название организации')
+    name_event = forms.CharField(label='мероприятие')
+    date_event = forms.DateField(label='дата проведения', widget=forms.SelectDateWidget)
+    deadline = forms.DateField(label='запись до', widget=forms.SelectDateWidget)
+    name = forms.CharField(label='имя отвественнго')
+    surname = forms.CharField(label='фамилия отвественнго')
+    email = forms.EmailField(label='электронный адрес')
+    folder = forms.CharField(label='папка для хранения')
+
--- app/eventapp/models.py
+++ app/eventapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+import datetime
 # Create your models here.
+
+
+class Events(models.Model):
+    name = models.TextField(null=False)
+    created = models.DateField(null=False, default=datetime.date.today)
+    status = models.CharField(max_length=20, default='создан', null=False)
+    folder = models.TextField(null=False)
+class Organizer(models.Model):
+    event = models.ForeignKey(Events, on_delete=models.CASCADE, null=False)
+    name = models.CharField(max_length=20, null=False)
+    surname = models.CharField(max_length=50, null=False)
+    email = models.EmailField(null=False)
+class Deadline(models.Model):
+    event = models.ForeignKey(Events, on_delete=models.CASCADE, null=False)
+    date = models.DateField(null=False)
